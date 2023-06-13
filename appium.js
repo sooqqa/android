e5a28f69fa531b707f7bc179f0423753ecc8dd0f
sooqqa <78123350+sooqqa@users.noreply.github.com>
2023-06-13 20:23:05 +0000

    Added message sending and create new client functions.

--- appium.js
+++ appium.js
@@ -10,13 +10,13 @@ async function runCalculatorTest() {
       'appium:deviceName': 'Nexus 6',
       'appium:appPackage': 'com.whatsapp',
       'appium:appActivity': 'com.whatsapp.Main',
-      "appium:automationName": 'UiAutomator2',
+      "appium:automationName": 'uiautomator2',
       'appium:noReset': true,
       'appium:fullReset': false,
     }
   });
 
-//   console.log('YYYYYYEEEEEEEEEESSSSSS, BLEATSSS');
+  console.log('YYYYYYEEEEEEEEEEAAAAAASSSSSS, BLEATSSS');
 
 
     //Step 1:
@@ -32,69 +32,119 @@ async function runCalculatorTest() {
 
     //Step3:
     // await driver.$('android=new UiSelector().text("United States")').click();
-    // await (await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/menuitem_search")')).click();
+    // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/menuitem_search")').click();
     // await driver.$('android= new UiSelector().resourceId("com.whatsapp:id/search_src_text")').setValue('Ukraine');
     // await new Promise((resolve) => setTimeout(resolve, 5000));
     // const country = await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/country_first_name")');
     // await country.click();
     // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/registration_phone")').setValue("688400671");
     // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/registration_submit")').click();
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
+    // await driver.$('android=new UiSelector().resourceId("android:id/button1")').click();
     // await new Promise((resolve) => setTimeout(resolve, 15000));
     // executeADBCommand(`exec-out screencap -p > screenshotStep3.png`);
-    // await driver.$('android=new UiSelector().resourceId("android:id/button1")').click();
 
 
     //Step4:
     //Sms code
-    // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/verify_sms_code_input")').setValue('363731');
+    // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/verify_sms_code_input")').setValue('595131');
 
     //Permissions (use only new accounts and new emulator)
     // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/submit")').click();
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
     // await driver.$('android=new UiSelector().text("Allow")').click();
+    // await new Promise((resolve) => setTimeout(resolve, 5000));  //2x maybe
     // await driver.$('android= new UiSelector().text("Skip")').click();
+    // await new Promise((resolve) => setTimeout(resolve, 10000));
 
     //Added info to user
-    // const userName = 'Andriy';
+    // const userName = 'Spammer';
     // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/registration_name")').setValue(userName);
     // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/register_name_accept")').click();
+    // await new Promise((resolve) => setTimeout(resolve, 10000));
+
+    const client_name = "Taras";
+    const client_last_name = "Client2";
+    const client_phone = "663895605";
+    const message = 'Привіт, ми тут';
+
+    async function reloadApp() {
+      await driver.closeApp();
+      await new Promise((resolve) => setTimeout(resolve, 30000));
+      await driver.launchApp();
+      await new Promise((resolve) => setTimeout(resolve, 30000));
+    }
+
+    async function addNewClient(client_name, client_last_name, client_phone) {
+      try{
+        await (await driver.$('android=new UiSelector().text("New contact")')).click();
+        await new Promise((resolve) => setTimeout(resolve, 25000));
+        const first_name = await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/first_name_field")');
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        await first_name.click();
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        await first_name.setValue(client_name);
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        const last_name = await driver.$('android=new UiSelector().text("Last name")');
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        await last_name.click();
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        await last_name.setValue(client_last_name);
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        await (await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/country_code_field")')).click();
+        await new Promise((resolve) => setTimeout(resolve, 30000));
+        await (await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/menuitem_search")')).click();
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        await (await driver.$('android= new UiSelector().resourceId("com.whatsapp:id/search_src_text")')).setValue('Ukraine');
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        const country = await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/country_first_name")');
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        await country.click();
+        await new Promise((resolve) => setTimeout(resolve, 30000));
+        await (await driver.$('android=new UiSelector().text("Phone")')).setValue(client_phone);
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        await (await driver.$('android=new UiSelector().text("SAVE")')).click();
+        await new Promise((resolve) => setTimeout(resolve, 30000));
+      } catch {
+        console.log('User not been added');
+      }
+    }
+
+    async function sendMessage(client_phone, message) {
+      try{
+        await (await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/fab")')).click();
+      } catch {
+        try {
+          await (await driver.$('android=new UiSelector().text("Send message")')).click();
+        } catch {
+          console.log('Element not find, back to main app');
+        }
+        console.log('Element not find, back to main app');
+      }
+      await new Promise((resolve) => setTimeout(resolve, 15000));
+      await (await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/menuitem_search")')).click();
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      await (await driver.$('android=new UiSelector().text("Search name or number…")')).setValue(client_phone);
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      await (await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/contactpicker_text_container")')).click();
+      await new Promise((resolve) => setTimeout(resolve, 15000));
+      await (await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/entry")')).setValue(message);
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      await (await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/send")')).click();
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
 
 
-
-    //Send sms to new client
-    const client_name = "Test";
-    const client_last_name = "Test";
-    const client_phone = "678446760"
-
-    // await driver.$('android=new UiSelector().text("Send message")').click();
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-    // await driver.$('android=new UiSelector().text("New contact")').click();
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-    // const first_name = await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/first_name_field")');
-    // await first_name.click();
-    // await first_name.setValue(client_name);
-    // const last_name = await driver.$('android=new UiSelector().text("Last name")');
-    // await last_name.click();
-    // await last_name.setValue(client_last_name);
-    // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/country_code_field")').click();
-
-    // await (await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/menuitem_search")')).click();
-    // await driver.$('android= new UiSelector().resourceId("com.whatsapp:id/search_src_text")').setValue('Ukraine');
-    // await new Promise((resolve) => setTimeout(resolve, 5000));
-    // const country = await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/country_first_name")');
-    // await country.click();
-    // await driver.$('android=new UiSelector().text("Phone")').setValue(client_phone);
-    // await driver.$('android=new UiSelector().text("SAVE")').click();
-    //await driver.$('android=new UiSelector().description("Navigate up")').click();
-
-
-    
-
+    // await driver.$('android=new UiSelector().description("Navigate up")').click();
 
     //get All Xml on the page
     // console.log(await driver.getPageSource());
 
+    // await reloadApp();
 
+    // await sendMessage('380'+client_phone, message);
 
+    // await addNewClient(client_name, client_last_name, client_phone);
 
     //Screenshot
     await new Promise((resolve) => setTimeout(resolve, 3000));
