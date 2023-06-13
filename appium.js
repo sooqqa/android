488ba8760a88433cedaaf8c5603db87d8840e207
skripchak <90845291+skripchak@users.noreply.github.com>
2023-06-13 03:59:38 +0000

    Fixed auth in the whatsapp.

--- appium.js
+++ appium.js
@@ -22,27 +22,47 @@ async function runCalculatorTest() {
     //Step 1:
     // const buttonElement = (await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/next_button")')).click();
 
+    // await new Promise((resolve) => setTimeout(resolve, 15000));
+    // executeADBCommand(`exec-out screencap -p > screenshotStep1.png`);
+
     //Step2:
     // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/eula_accept")').click();
+    // await new Promise((resolve) => setTimeout(resolve, 15000));
+    // executeADBCommand(`exec-out screencap -p > screenshotStep2.png`);
 
     //Step3:
     // await driver.$('android=new UiSelector().text("United States")').click();
     // await (await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/menuitem_search")')).click();
     // await driver.$('android= new UiSelector().resourceId("com.whatsapp:id/search_src_text")').setValue('Ukraine');
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
     // const country = await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/country_first_name")');
     // await country.click();
     // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/registration_phone")').setValue("688400671");
     // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/registration_submit")').click();
+    // await new Promise((resolve) => setTimeout(resolve, 15000));
+    // executeADBCommand(`exec-out screencap -p > screenshotStep3.png`);
     // await driver.$('android=new UiSelector().resourceId("android:id/button1")').click();
 
 
     //Step4:
     //Sms code
-    // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/verify_sms_code_input")').setValue('188964');
+    // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/verify_sms_code_input")').setValue('363731');
 
-    //Permissions (use only new accounts)
+    //Permissions (use only new accounts and new emulator)
     // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/submit")').click();
     // await driver.$('android=new UiSelector().text("Allow")').click();
+    // await driver.$('android= new UiSelector().text("Skip")').click();
+
+    //Added info to user
+    // const userName = 'Andriy';
+    // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/registration_name")').setValue(userName);
+    // await driver.$('android=new UiSelector().resourceId("com.whatsapp:id/register_name_accept")').click();
+
+
+
+
+    // await driver.$('android=new UiSelector().text("Verify")').click();
+
 
     
 
@@ -54,8 +74,8 @@ async function runCalculatorTest() {
 
 
     //Screenshot
-    // await new Promise((resolve) => setTimeout(resolve, 5000));
-    executeADBCommand(`exec-out screencap -p > screenshotStep5.png`);
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+    executeADBCommand(`exec-out screencap -p > screenshotStep.png`);
   await driver.deleteSession();
 }
 
